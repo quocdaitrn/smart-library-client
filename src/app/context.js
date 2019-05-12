8b2aafec84f1bd1eb7cc18f7fd131971630ff67c
DaiTran <daitq.cntt@gmail.com>
2019-05-12 11:29:00 +0700

    remove redundant dependencies

--- src/app/context.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const state = {
-    loginVisible: false,
-    signupVisible: false,
-};
-export const AppContext = React.createContext(state); 
--- src/components/common/header/AppHeader.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-class AppHeader extends React.Component {
-
-}
